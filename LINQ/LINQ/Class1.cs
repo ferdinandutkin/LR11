3432062b0ec7eb771d5216b033cedde7a26b02a2
ferdinandutkin <61806555+ferdinandutkin@users.noreply.github.com>
2020-11-16 14:25:20 +0300

    Add files via upload

---
+++ LINQ/LINQ/Class1.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace LINQ
+{
+
+    static class StringExtensions
+    {
+        public static string ToNameCase(this string source) => source.ToLower().Remove(0, 1)
+                    .Insert(0, source.Substring(0, 1).ToUpper());
+
+    }
+
+    public class Date
+    {
+        private ushort day, month;
+        public ushort Day
+        {
+            get => day;
+            set
+            {
+                if (value > 31)
+                {
+                    day = 31;
+                }
+                else
+                {
+                    day = value;
+                }
+            }
+        }
+        public ushort Month
+        {
+            get => month;
+            set
+            {
+                if (value > 12)
+                {
+                    month = 12;
+                }
+                else
+                {
+                    month = value;
+                }
+            }
+        }
+        public int Year { get; set; }
+
+
+        public Date(string date)
+        {
+            string[] tokens = date.Split('.', StringSplitOptions.RemoveEmptyEntries);
+            if (tokens.Length != 3)
+            {
+                Debug.WriteLine("парсинг даты сломался");
+            }
+            else
+            {
+                Day = ushort.Parse(tokens[0]);
+                Month = ushort.Parse(tokens[1]);
+                Year = int.Parse(tokens[2]);
+
+            }
+
+        }
+
+        public Date(ushort day, ushort month, int year) => (Day, Month, Year) = (day, month, year);
+
+ 
+
+
+        public static implicit operator Date(string s) => new Date(s);
+
+        public override string ToString() => $"{Day}.{Month}.{Year}";
+
+        public override bool Equals(object o) => (o is Date date) &&
+            (date.Day, date.Month, date.Year) == (Day, Month, Year);
+       
+        public override int GetHashCode() => HashCode.Combine(Day, Month, Year);
+    }
+
+    public class Person : IComparable<Person>
+    {
+        private string surname;
+        private string name;
+        private string patronymic;
+
+
+        public string Surname
+        {
+            get => surname;
+            set => surname = value.ToNameCase();
+        }
+
+        public string Name
+        {
+            get => name;
+            set => name = value.ToNameCase();
+        }
+
+        public string Patronymic
+        {
+            get => patronymic;
+            set => patronymic = value.ToNameCase();
+        }
+
+        public string FullName => $"{Surname} {Name} {Patronymic}";
+
+        public ushort Age =>
+              (ushort)((DateTime.Now.Year - Birthday.Year - 1) + (((DateTime.Now.Month > Birthday.Month)
+            || ((DateTime.Now.Month == Birthday.Month) && (DateTime.Now.Day >= Birthday.Day))) ? 1 : 0));
+
+        public Date Birthday { get; protected set; }
+
+
+        public Person(string surname, string name, string patronymic, Date birthday)
+        {
+            Surname = surname;
+            Name = name;
+            Patronymic = patronymic;
+            Birthday = birthday;
+        }
+
+        //в этом языке не любят copy ctorы поэтому пускай будет protected)))
+        protected Person(Person p) : this(p.Surname, p.Name, p.Patronymic, p.Birthday)
+        {
+        }
+
+        public Person(string personaldata)
+        {
+            string[] pd = personaldata.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            if (pd.Length != 4)
+            {
+                Debug.WriteLine("парсинг личных данных сломался");
+            }
+            else
+            {
+                Surname = pd[0];
+                Name = pd[1];
+                Patronymic = pd[2];
+                Birthday = pd[3];
+
+            }
+
+        }
+
+
+        public override string ToString() => FullName + $" {Birthday}";
+
+        public override bool Equals(object o)
+        {
+            if ((o == null) || !GetType().Equals(o.GetType()))
+            {
+                return false;
+            }
+            else
+            {
+                var p = (Person)o;
+                return (p.Name == Name) && (p.Patronymic == Patronymic) && (p.Surname == Surname) && p.Birthday.Equals(Birthday);
+            }
+        }
+
+        public override int GetHashCode() => HashCode.Combine(Surname, Name, Patronymic, Birthday);
+
+        public int CompareTo(Person other) => Age.CompareTo(other.Age);
+       
+    }
+
+    public class Student : Person
+    {
+        static string infoTemplate;
+        static int currentCount;
+        public static string Info
+          => string.Format(infoTemplate, typeof(Student).Name, currentCount);
+
+        public readonly int id;
+
+
+
+        private string phoneNumber;
+
+
+
+        static Student()
+        {
+            currentCount = 0;
+            infoTemplate = "Класс {0}, на данный момент насчитывающий {1} объектов";
+        }
+
+        public override string ToString() => base.ToString() + $" {Faculty} {Course} {Group}";
+
+        //  Адрес,  Телефон, Факультет, Курс, Группа.
+
+
+        public string Address
+        {
+            get; set;
+        }
+
+        public string PhoneNumber
+        {
+            get => phoneNumber;
+            set
+            {
+                var toSet = value.Trim();
+                if (toSet.Length != 13)
+                {
+                    Debug.WriteLine("номер должен быть 13 символов");
+                }
+                else
+                {
+                    phoneNumber = toSet;
+                }
+
+            }
+        }
+
+        public string Faculty
+        {
+            get; set;
+        }
+        public int Course
+        {
+            get; set;
+        }
+
+        public int Group
+        {
+            get; set;
+        }
+
+        public override bool Equals(object o)
+        {
+
+            if ((o == null) || !GetType().Equals(o.GetType()))
+            {
+                return false;
+            }
+            else
+            {
+                var s = (Student)o;
+                return base.Equals(s) && (s.Address == Address) && (s.PhoneNumber == PhoneNumber) && (s.Faculty == Faculty) && (s.Course == Course) && (s.Group == Group);
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            HashCode hash = new HashCode();
+            hash.Add(base.GetHashCode());
+            hash.Add(Address);
+            hash.Add(PhoneNumber);
+            hash.Add(Faculty);
+            hash.Add(Course);
+            hash.Add(Group);
+            return hash.ToHashCode();
+        }
+
+        private Student(Person p, string address, string phonenumber, string faculty, int course = 1, int group = 1) : base(p)
+        {
+            Address = address;
+            PhoneNumber = phonenumber;
+            Faculty = faculty;
+            Course = course;
+            Group = group;
+            currentCount++;
+            id = Math.Abs(GetHashCode());
+        }
+        //аргументы по умолчанию есть! приватный конструктор есть!
+        public Student(string personaldata, string address, string phonenumber, string faculty, int course, int group) :
+            this(new Person(personaldata), address, phonenumber, faculty, course, group)
+        {
+        }
+
+        public Student() : this(new Person("Неизвестно Неизвестно Неизвестно 01.01.1970"), "Неизвестно", "Неизвестно", "Неизвестно")
+        {
+        }
+
+        ~Student() => currentCount--;
+
+    }
+}
---
+++ LINQ/LINQ/Program.cs
@@ -0,0 +1,166 @@
+﻿ 
+using System;
+using System.Globalization;
+using System.Linq;
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            var months = new[] {"January", "February","March","April",
+                    "May","June","July","August","September","October","November","December"}.OrderBy(el => random.Next()).ToArray();
+
+
+            //1.1
+            var n = 7;
+            var monthQuery = months.Where(month => month.Length == n);
+
+
+
+            foreach (var month in monthQuery)
+            {
+                Console.WriteLine(month);
+            }
+
+            //1.2
+
+            Predicate<int> isWinterMonth = number => number == 1 || number == 2 || number == 12;
+
+            Predicate<int> isSummerMonth = number => number == 6 || number == 7 || number == 8;
+
+            var seasonQuery = from month in months
+                              let monthNumber = Array.IndexOf(CultureInfo.InvariantCulture.DateTimeFormat.MonthNames, month) + 1
+                              where isWinterMonth(monthNumber) || isSummerMonth(monthNumber)
+                              select month;
+
+            foreach (var month in seasonQuery)
+            {
+                Console.WriteLine(month);
+            }
+
+            //1.3
+
+
+            var monthUQuery = months.Where(month => month.Length >= 4 && month.Contains('u'));
+
+            foreach (var month in monthUQuery)
+            {
+                Console.WriteLine(month);
+            }
+
+
+            //2
+            var students = new[]
+            {
+                new Student("тумаш станислав игоревич 22.08.2001", "улица блаблабла", "+375255377984", "ФИТ", 2, 1),
+                new Student("савельев талан михайлович 03.07.1998", "улица1 блаблабла", "+375445938865", "ХТиТ", 2, 3),
+                new Student("стельмаков константин ЕВГЕНЬЕВИЧ 14.06.2001","улица2 блаблабла", "+375335785843", "ФИТ", 2, 4),
+                new Student("василевич владимир павлович 21.03.2000", "улица3 блаблабла", "+375255377994", "ФИТ", 2, 3),
+                new Student("удовыдченкова анастасия александровна 12.01.2001", "улица4 блаблабла", "+375334845667", "ФИТ", 2, 9),
+                new Student("карпенкин         дмитрий анатольевич 28.04.1999", "улица5 блаблабла", "+375257359612", "ФИТ", 2, 4),
+                new Student("дежемесов александр егорович 29.12.2001", "улица6 блаблабла", "+375335785968", "ФИТ", 2, 3),
+                new Student("челик какой-то нормальный 9.11.1996", "улица7 блаблабла", "+375335785968", "ФИТ", 2, 3),
+                new Student("пулатов александр егорович 29.12.2001", "улица8 блаблабла", "+375335987777", "ФИТ", 2, 2),
+                new Student("пулатов михаил егорович 29.12.2001", "улица9 блаблабла", "+375335537769", "ФИТ", 1, 3),
+                new Student("липенко дмитрий павлович 220.10.2000", "улица10 блаблабла", "+375257359612", "ХТиТ", 2, 4),
+            };
+
+
+            //3.1
+            var faculty = "фит";
+            var studentFacultyQuery = students
+                .Where(student => string.Equals(student.Faculty, faculty, StringComparison.OrdinalIgnoreCase))
+                .OrderBy(student => student.FullName.First());
+
+            foreach (var student in studentFacultyQuery)
+            {
+                Console.WriteLine(student);
+            }
+
+            //3.2
+
+            var group = 3;
+
+            var studentGroupFacultyQuery = students
+              .Where(student => string.Equals(student.Faculty, faculty, StringComparison.OrdinalIgnoreCase) && student.Group == group);
+
+            foreach (var student in studentGroupFacultyQuery)
+            {
+                Console.WriteLine(student);
+            }
+
+            //3.3
+
+            var youngestStudent = students.Min();
+
+            Console.WriteLine(youngestStudent);
+
+            //3.4
+
+            var studentGroupQuery = students
+                .Where(student => student.Group == group)
+                .OrderBy(student => student.Surname.First());
+ 
+            foreach (var student in studentGroupQuery)
+            {
+                Console.WriteLine(student);
+            }
+
+            //3.5
+            var name = "дмитрий";
+            var firstWithName = students.First(student => string.Equals(student.Name, name, StringComparison.OrdinalIgnoreCase));
+
+            Console.WriteLine(firstWithName);
+
+
+
+
+
+
+
+
+
+            //4-5
+            //минимальный возраст в каждой группе 2 курса по убыванию возраста
+
+            var groupAge =
+                from GA in
+                    from student in students
+                    where student.Course == 2
+                    group student by student.Group into SG
+
+                    select new
+                    {
+                        Group = SG.Key,
+                        Age = SG.Min(student => student.Age)
+                    }
+                orderby GA.Age descending
+                select GA;
+
+            //является ли в хотя бы 1 группе 19 минимальным возрастом
+            bool isAny = groupAge.Any(GA => GA.Age == 17);
+
+            //пары студент минимальный возраст в его группе
+            var studentGroupAge = (from student in students
+                                   join GA in groupAge on student.Group equals GA.Group
+                                   select new
+                                   {
+                                       Student = student,
+                                       GA.Age
+                                   }).Skip(2); //Skip чтоб было
+
+            
+           
+            
+          
+
+
+
+                               
+        }
+    }
+
+}
